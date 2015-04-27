75e8182f1994b7036b4f2008fd06f9d1f15082fb
james robert williams & Paul Ogawa <student@epicodus.com>
2015-04-27 14:20:57 -0700

    it adds logic for weight based shipping and an error for being over 70lbs all tests passing

--- lib/parcels.rb
+++ lib/parcels.rb
@@ -5,9 +5,28 @@ class Parcels
     @heighth = heighth
     @weight = weight
   end
+
   define_method(:volume) do
     volume = @length * @width *  @heighth
     volume
+  end
 
+
+  def oversize
+
+  end
+
+
+  def cost_to_ship
+
+      shipping_cost = 0
+
+      if @weight >= 70
+        "The package is to heavy to ship USPS, please use a frieght service"
+      elsif @weight < 2
+        shipping_cost = shipping_cost + 1
+      else @weight < 10
+        shipping_cost = shipping_cost + 2
+      end
   end
 end
--- spec/parcels_spec.rb
+++ spec/parcels_spec.rb
@@ -5,10 +5,23 @@ require "rerun"
 
 
 describe(Parcels) do
-  describe('Parcels')do
+  describe('volume')do
     it('returns the volume of a parcel') do
       test_parcel = Parcels.new(10,15,20,25)
       expect(test_parcel.volume()).to(eq(3000))
+    end
+  end
+
+  describe('cost_to_ship') do
+    it('returns the cost to ship a package based off information given') do
+      test_parcel = Parcels.new(10,15,20,25)
+      expect(test_parcel.cost_to_ship()).to(eq(2))
+    end
+  end
+  describe('cost_to_ship') do
+    it('returns an error message if the package is over 70 lbs') do
+      test_parcel = Parcels.new(10,15,20,70)
+      expect(test_parcel.cost_to_ship()).to(eq("The package is to heavy to ship USPS, please use a frieght service"))
     end
   end
 
