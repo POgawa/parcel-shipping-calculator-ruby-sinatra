550291c877e0fb06115537f226fe69cb253d8690
james robert williams & Paul Ogawa <student@epicodus.com>
2015-04-27 16:03:17 -0700

    update sinatra conditional for 0 measurements to return alternate messaging

--- lib/parcels.rb
+++ lib/parcels.rb
@@ -24,7 +24,9 @@ class Parcels
 
       shipping_cost = 0
 
-      if @weight >= 70
+      if @weight == 0
+        "Please enter valid weight"
+      elsif @weight >= 70
         shipping_cost = "The package is to heavy to ship USPS, please use a frieght service"
       elsif @weight < 2
         shipping_cost = shipping_cost + 1
