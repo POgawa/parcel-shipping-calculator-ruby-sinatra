614ee35084dcd87bca5f60133e2bf1049d84d2b8
james robert williams & Paul Ogawa <student@epicodus.com>
2015-04-27 13:58:32 -0700

    add volume method to calculate the volume of a parcel/package

--- lib/parcels.rb
+++ lib/parcels.rb
@@ -1,6 +1,13 @@
 class Parcels
+  define_method(:initialize) do |length, width, heighth, weight|
+    @length = length
+    @width = width
+    @heighth = heighth
+    @weight = weight
+  end
+  define_method(:volume) do
+    volume = @length * @width *  @heighth
+    volume
 
-
-
-
+  end
 end
