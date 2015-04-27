00d522c361f78fbfa558bba1b405c09029f5020a
james robert williams & Paul Ogawa <student@epicodus.com>
2015-04-27 15:39:32 -0700

    it adds passing capybara test

--- app.rb
+++ app.rb
@@ -6,3 +6,16 @@ require "rerun"
 get('/') do
    erb(:index)
 end
+
+get('/results') do
+
+  @length = params.fetch('length')
+  @width = params.fetch('width')
+  @heigth = params.fetch('height')
+  @weight = params.fetch('weight')
+  
+
+
+  erb(:results)
+
+end
--- spec/parcels_integration_spec.rb
+++ spec/parcels_integration_spec.rb
@@ -2,3 +2,15 @@ require ('parcels.rb')
 require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
+
+describe('calculate shipping path') do
+  it('takes the form entry and shows the result page', type: 'feature') do
+    visit('/')
+    fill_in('width', with: '12')
+    fill_in('length', with: '15')
+    fill_in('heigth', with: '15')
+    fill_in('weight', with: '20')
+    click_button('Calculate!')
+    expect(page).to have_content("Your shipping cost!")
+  end
+end
